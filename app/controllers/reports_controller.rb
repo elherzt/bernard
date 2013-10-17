c1e3109e874c4c2cc43b2aa074bc9bac05651d59
Alejandra Cernas <alejandra.cernas@tangosource.com>
2013-10-17 14:50:07 -0500

    Created json for Chart.js

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,11 +6,26 @@ class ReportsController < ApplicationController
   end
 
   def income_v_expense
+    @transactions = Transaction.get_income_outcome
+    
+    respond_to do |format|
+      format.json { render json: { :labels => format_data(:income),
+                                   :datasets => [ 
+                                                 { :data => format_data(:income)},
+                                                 { :data => format_data(:outcome)}
+                                                ]  } }
+    end
   end
 
   def spending_by_payee
   end
 
   def spending_by_category
+  end
+
+  private
+
+  def format_data(data)
+    @transactions.map(&data)
   end
 end
